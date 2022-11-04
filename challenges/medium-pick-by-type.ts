2169665d1253feed889a811f771ce487cfc9b9e8
CodeByZack <958059970@qq.com>
2022-11-04 17:38:49 +0800

    1 file added

---
+++ challenges/medium-pick-by-type.ts
@@ -0,0 +1,30 @@
+/*******challenges-description*****   
+From `T`, pick a set of properties whose type are assignable to `U`.
+
+For Example
+
+```typescript
+type OnlyBoolean = PickByType<{
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}, boolean> // { isReadonly: boolean; isEnable: boolean; }
+```
+********************************/
+
+type PickByType<T, U> = any
+
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}
+
+type cases = [
+  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>,
+]
+
